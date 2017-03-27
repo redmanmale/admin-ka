d41966d302047e5d39b86316d769916a6877e595
redmanmale <redmanmale@gmail.com>
2017-03-27 23:52:22 +0300

    Updated mock data

--- DemoServer/server.js
+++ DemoServer/server.js
@@ -3,18 +3,25 @@ var express = require('express');
 for(var i = 0; i < 4; i++) (function(i) {
     var app = express();
     var fs = require("fs");
-    
-    app.get('/stats', function (req, res) {        
-        fs.readFile( __dirname + "/" + 'data-' + i + '.json', 'utf8', function (err, data) {            
+
+    app.get('/stats', function (req, res) {
+        fs.readFile( __dirname + "/" + 'stats-' + i + '.json', 'utf8', function (err, data) {
             res.end(data);
             console.log('Send stats');
+        });
+    })
+
+    app.get('/info', function (req, res) {
+        fs.readFile( __dirname + "/" + 'info-' + i + '.json', 'utf8', function (err, data) {
+            res.end(data);
+            console.log('Send info');
         });
     })
 
     var server = app.listen(8083 + i, '127.0.0.1', function () {
         var host = server.address().address
         var port = server.address().port
-        
+
         console.log("Listening at http://%s:%s", host, port)
     });
 }) (i);
